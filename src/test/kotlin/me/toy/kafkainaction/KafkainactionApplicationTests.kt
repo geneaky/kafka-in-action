cc72d61e1e7fa47f9efb0497c4ddbdd86b00cab4
channy <changryun.ju@waiker.ai>
2025-01-31 16:25:15 +0900

    first commit

---
+++ src/test/kotlin/me/toy/kafkainaction/KafkainactionApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.toy.kafkainaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkainactionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
